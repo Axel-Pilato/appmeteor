11ec0f796cb7e38ac2db5f82ca11d9f49c7f46c3
Axel <axelpilato@gmail.com>
2019-03-14 14:33:16 +0100

    Ajout de la page Artiste

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,8 +4,10 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/artistes/artistes.js';
 import '../../ui/pages/form/form.js';
 import '../../ui/pages/not-found/not-found.js';
+
 
 // Set up all routes in the app
 FlowRouter.route('/', {
@@ -15,7 +17,7 @@ FlowRouter.route('/', {
   },
 });
 
-
+// Page de formulaire & liste des artistes
 FlowRouter.route('/form', {
   name: 'App.form',
   action() {
@@ -23,7 +25,15 @@ FlowRouter.route('/form', {
   },
 });
 
+// Page descriptive pour chaque artiste
+FlowRouter.route('/artistes/:id', {
+  name: 'App.artistes',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_artistes' });
+  },
+});
 
+// 404 - Not Found
 FlowRouter.notFound = {
   action() {
     BlazeLayout.render('App_body', { main: 'App_notFound' });
---
+++ imports/ui/pages/artistes/artistes.js
@@ -0,0 +1,4 @@
+import '../../components/navigation/navigation.js';
+import '../../components/footer/footer.js';
+import '../../components/pageArtiste/pageArtiste.js';
+import './artistes.html';
