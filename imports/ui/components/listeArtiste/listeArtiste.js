53c5e4f1a07b4c95f3ffa57c35306ee4547b36a2
Axel <axelpilato@gmail.com>
2019-03-15 10:50:47 +0100

    Ajout des réseaux + image

--- imports/ui/components/listeArtiste/listeArtiste.js
+++ imports/ui/components/listeArtiste/listeArtiste.js
@@ -4,18 +4,6 @@ import { FilesCollection } from 'meteor/ostrio:files';
 import './listeArtiste.html';
 
 
-const Images = new FilesCollection({
-  Artiste: 'Images',
-  onBeforeUpload(file) {
-
-    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
-      return true;
-    }
-    return 'Please upload image, with size equal or less than 10MB';
-  }
-});
-
 Template.listeArtiste.onCreated(function () {
   Meteor.subscribe('artiste.all');
 });
